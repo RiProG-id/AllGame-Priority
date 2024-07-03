4ac2579f6a7ffd8804700320d95a00af6fda38ae
RiProG-ID <muhammadrizki01032003@gmail.com>
2024-07-03 20:48:42 +0800

    Add game detection and filter

--- NR/Priority/run.sh
+++ NR/Priority/run.sh
@@ -21,7 +21,22 @@ echo "AllGame Priority 3.0"
 echo ""
 
 maindir=/sdcard/Priority
+
 sleep 2
+
+counter=1
+package_list=$(pm list packages | cut -f 2 -d :)
+game_list=$(cat "$maindir/gamelist.txt")
+echo "$game_list" | while IFS= read -r gamelist; do
+    line=$(echo "$gamelist" | awk '!/ /')
+    if echo "$package_list" | grep -q "$line"; then
+        echo "  $counter. $line"
+        counter=$((counter + 1))
+    else
+        sed -i "/$line/d" "$maindir/gamelist.txt"
+    fi
+done
+echo ""
 
 if [ -f "$maindir/toast.apk" ]; then
   if ! pm list packages -3 | grep -q bellavita.toast; then
